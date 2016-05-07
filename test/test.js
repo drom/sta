21856994e38592630be6bf46e1a9cb279574dfdc
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-05-07 00:18:42 -0700

    initial code

---
+++ test/test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var onml = require('onml'),
+    path1 = require('./path1'),
+    lib = require('../lib');
+
+describe('test', function () {
+    it('path1', function (done) {
+        var src = [];
+        var res = lib.render(path1);
+        var svg = onml.s(res);
+        console.log(svg);
+        done();
+    });
+});
