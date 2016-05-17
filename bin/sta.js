450b9f558959ef56d5c6c0df07786521d5adf553
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-05-16 22:45:24 -0700

    added icestorm support

--- bin/sta.js
+++ bin/sta.js
@@ -6,12 +6,15 @@ var lib = require('../lib'),
     yargs = require('yargs'),
     onml = require('onml');
 
-var argv = yargs.argv;
+var argv = yargs.count('icestorm').argv;
 var fileName;
 
 if (argv._.length === 1) {
     fileName = argv._[0];
     fs.readJson(fileName, function (err, src) {
+        if (argv.icestorm) {
+            var src = lib.icestorm.import(src);
+        }
         var res = lib.render(src);
         var svg = onml.s(res);
         console.log(svg);
