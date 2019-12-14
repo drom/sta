7966cab7e3295cdc17b9ca4fa1a1c6a3a8f55195
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2019-12-14 00:08:12 -0800

    more linting, chai tests

--- test/test.js
+++ test/test.js
@@ -3,12 +3,18 @@
 var onml = require('onml'),
     path1 = require('./path1'),
     lib = require('../lib');
+var chai = require('chai');
+
+var expect = chai.expect;
 
 describe('test', function () {
     it('path1', function (done) {
-        var src = [];
         var res = lib.render(path1);
+        expect(res).to.be.an('array');
         var svg = onml.s(res);
+        expect(svg).to.be.an('string');
         done();
     });
 });
+
+/* eslint-env mocha */
